e174cfaafa6f21fc8351130f4be1e128c51f09b9
Tryton Van Meer <code@trytonvanmeer.dev>
2024-02-20 17:11:52 -0500

    feat: implement list command
    walk the CWD and attempt read all files found.

--- src/cli.rs
+++ src/cli.rs
@@ -4,6 +4,7 @@ use anyhow::Result;
 use clap::{Command, CommandFactory, Parser, Subcommand};
 use clap_complete::{generate, Shell};
 use colored::Colorize;
+use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None, subcommand_negates_reqs = true)]
@@ -22,6 +23,8 @@ enum Commands {
         #[clap(value_enum)]
         shell: Option<Shell>,
     },
+    /// Walk over all files in the current directory
+    List {},
 }
 
 fn generate_completions(shell: Shell, cmd: &mut Command) -> Result<()> {
@@ -31,6 +34,26 @@ fn generate_completions(shell: Shell, cmd: &mut Command) -> Result<()> {
         cmd.get_name().to_string(),
         &mut std::io::stdout(),
     );
+
+    Ok(())
+}
+
+fn list_cmd() -> Result<()> {
+    for entry in WalkDir::new(".").into_iter().flatten() {
+        if entry.metadata()?.is_file() {
+            let path = entry.path().to_str().unwrap();
+            let video = Video::new(path);
+
+            match video {
+                Ok(video) => println!(
+                    "{}\t{}",
+                    video.codec.green().bold(),
+                    video.filename.purple().bold()
+                ),
+                Err(error) => eprintln!("{}: {}: {}", "Error".red(), path, error),
+            };
+        }
+    }
 
     Ok(())
 }
@@ -65,6 +88,7 @@ pub fn run() -> Result<()> {
 
                 generate_completions(gen, &mut Cli::command())?
             }
+            Commands::List {} => list_cmd()?,
         }
     } else {
         default_cmd(args.path.unwrap())?
